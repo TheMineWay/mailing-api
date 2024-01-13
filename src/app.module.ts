baa4ff32160654b4910b784193640b18c5585960
Joel Campos <joelcamposoliva9000@gmail.com>
2024-01-13 21:49:47 +0100

    Fetch and organise agents

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { ApiKeyGuard, ApiKeysModule } from 'nestjs-api-keys';
 import { ENV } from './constants/env/env.constant';
 import { APP_GUARD } from '@nestjs/core';
+import { MailAgentsModule } from './providers/mail-agents/mail-agents.module';
 
 const API_KEY_PERMISSION = 'api.use';
 
 @Module({
   imports: [
+    MailAgentsModule.forRoot(),
     ApiKeysModule.register({
       apiKeys: [
         {
---
+++ src/providers/mail-agents/mail-agents.module.ts
@@ -0,0 +1,36 @@
+import { DynamicModule, Logger, Module } from '@nestjs/common';
+import { MAIL_AGENTS_PROVIDER } from './mail-agents.provider';
+import { PrismaService } from '../../database/prisma.service';
+import { MailAgentRepository } from '../../database/repository/mail-agent.repository';
+import { reduce } from 'lodash';
+
+@Module({})
+export class MailAgentsModule {
+  static async forRoot(): Promise<DynamicModule> {
+    const repo = new MailAgentRepository(new PrismaService());
+
+    const agents = await repo.findAllIncludingMailServices();
+
+    const providers = [
+      {
+        provide: MAIL_AGENTS_PROVIDER,
+        useValue: reduce(
+          agents,
+          (result, value) => {
+            result[value.address] = result[value.address] || value;
+            return result;
+          },
+          {},
+        ),
+      },
+    ];
+
+    Logger.log('Initialized', 'Mail agents');
+
+    return {
+      module: MailAgentsModule,
+      providers,
+      exports: providers,
+    };
+  }
+}
