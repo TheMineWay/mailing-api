a3834c7aacac40f437451de7b148c209e7f3ac24
Joel Campos <joelcamposoliva9000@gmail.com>
2024-01-13 19:28:17 +0100

    Allow HTTPS

--- src/constants/env/env.constant.ts
+++ src/constants/env/env.constant.ts
@@ -1,4 +1,5 @@
 export const ENV = {
   port: +process.env.PORT ?? 1000,
   apiKeys: JSON.parse(process.env.API_KEYS ?? '[]') as string[],
+  https: process.env.HTTPS === 'false' ? false : true,
 };
--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,20 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ENV } from './constants/env/env.constant';
 import helmet from 'helmet';
+import fs from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(
+    AppModule,
+    ENV.https
+      ? {
+          httpsOptions: {
+            key: fs.readFileSync('/app/certificates/key.pem'),
+            cert: fs.readFileSync('/app/certificates/cert.pem'),
+          },
+        }
+      : {},
+  );
 
   app.enableCors();
   app.use(helmet());
