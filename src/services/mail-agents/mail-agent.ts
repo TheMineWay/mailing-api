38a0795775f8d502a695af1970f2114720fa3fe0
Joel Campos <joelcamposoliva9000@gmail.com>
2024-01-20 11:44:24 +0100

    Pick agent

--- src/services/mail-agents/mail-agent.ts
+++ src/services/mail-agents/mail-agent.ts
@@ -1,16 +1,26 @@
 import { MailAgents, MailServices } from '@prisma/client';
 import { SendEmailOptions } from '../../types/mail-agent/send-email.options.type';
+import { MailAgentType } from '../../types/mail-agent/mail-agent-type.enum';
+import { IMailAgent } from '../../types/mail-agent/mail-agent.interface';
+import { ResendMailAgent } from '../../types/mail-agent/agents/resend.mail-agent';
+import { InternalServerErrorException } from '@nestjs/common';
 
 export class MailAgent {
+  private readonly mailAgent: IMailAgent;
   constructor(
     private readonly mailAgentData: MailAgents,
     private readonly mailServiceData: MailServices,
-  ) {}
-
-  async sendEmail(options: SendEmailOptions) {
+  ) {
     switch (this.mailServiceData.type) {
-      case 'resend':
-        return null;
+      case MailAgentType.RESEND:
+        this.mailAgent = new ResendMailAgent(mailServiceData);
+        break;
+      default:
+        throw new InternalServerErrorException();
     }
+  }
+
+  async sendEmail(options: Omit<SendEmailOptions, 'from'>) {
+    this.mailAgent.send({ ...options, from: this.mailAgentData.address });
   }
 }
